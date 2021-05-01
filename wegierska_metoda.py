0fd7360893f0672f0a6d6c62d1d556f331b16d5f
Marek Lechowicz <m.lechow13@gmail.com>
2021-05-01 20:32:24 +0200

    pull

--- wegierska_metoda.py
+++ wegierska_metoda.py
@@ -21,10 +21,8 @@ class Matrix:
             for col in range(self.size):
                 self.matrix[row][col] -= min_value
 
-
     def reduce_matrix(self):
         reduction_sum = 0
-
         # reduce rows and update reduction sum
         self.reduce_and_get_sum()
 
@@ -32,6 +30,8 @@ class Matrix:
         self.matrix = self.matrix.T
         self.reduce_and_get_sum()
         self.matrix = self.matrix.T
+
+
 
     def min_lines(self):
         # DO POPRAWY poniższy kod  zadziała tylko w korzystnym przypadku, nie miałem lepszego pomysłu
@@ -69,14 +69,49 @@ class Matrix:
                 elif (row in self.independent_row) and (col in self.independent_col):
                     self.matrix[row, col] += min_element
 
+    # def get_total_cost(self):
+    #     optimal_points = []
+    #     total_cost = 0
+    #
+    #     chosen_col = []
+    #     chosen_row = []
+    #     for row in range(self.size):
+    #         minimal_zeros = np.inf
+    #         best_candidate = []
+    #         for col in range(self.size):
+    #             if (self.matrix[row, col] == 0) and (col not in chosen_col):
+    #                 number_of_zeros = 0
+    #                 # obliczenie liczby niewykreślonych zer w kolumnie
+    #                 for col_row in range(self.size):
+    #                     if (self.matrix[col_row, col] == 0) and (col_row not in chosen_row):
+    #                         number_of_zeros += 1
+    #                 if number_of_zeros < minimal_zeros:
+    #                     minimal_zeros = number_of_zeros
+    #                     best_candidate = [row, col]
+    #
+    #         optimal_points.append((best_candidate[0], best_candidate[1]))
+    #         total_cost += self.base_matrix[best_candidate[0], best_candidate[1]]
+    #         chosen_row.append(best_candidate[0])
+    #         chosen_col.append(best_candidate[1])
+    #
+    #     return optimal_points, total_cost
+
     def get_total_cost(self):
         optimal_points = []
         total_cost = 0
 
+        # uszeregownie kolejności wyboru wierszy
+        # od tych z najmniejszą liczbą zero do tych
+        # z największą
+        rows = list()
+        for i in range(self.size):
+            for row in range(self.size):
+                if list(self.matrix[row]).count(0) == i+1:
+                    rows.append(row)
+
         chosen_col = []
         chosen_row = []
-        for row in range(self.size):
-
+        for row in rows:
             minimal_zeros = np.inf
             best_candidate = []
             for col in range(self.size):
@@ -92,6 +127,8 @@ class Matrix:
 
             optimal_points.append((best_candidate[0], best_candidate[1]))
             total_cost += self.base_matrix[best_candidate[0], best_candidate[1]]
+            chosen_row.append(best_candidate[0])
+            chosen_col.append(best_candidate[1])
 
         return optimal_points, total_cost
 
@@ -171,13 +208,21 @@ if __name__ == '__main__':
     # print("\n\nMacierz końcowa: \n", matrix_example.matrix)
     # print("\n\nWynik", result)
 
-    matrix_example = []
-    for i in range(7):
-        matrix_example.append([])
-        for j in range(7):
-            matrix_example[i].append(randint(3, 15))
+    # matrix_example = []
+    # for i in range(7):
+    #     matrix_example.append([])
+    #     for j in range(7):
+    #         matrix_example[i].append(randint(3, 15))
+    #
+    # matrix_example = Matrix(matrix_example)
 
-    matrix_example = Matrix(matrix_example)
+    matrix_example = Matrix([[4,  6,  6,  5, 10,  6,  7],
+                             [7, 13, 10,  9, 15, 12, 14],
+                             [7, 13, 13, 13,  9, 12, 11],
+                             [0, 10,  6,  8,  5,  6, 12],
+                             [7,  4,  8, 15, 13, 11,  5],
+                             [3,  3,  4,  4,  4,  5, 10],
+                             [15, 12, 15, 14, 10, 12,  5]])
     print("Macierz początkowa: \n", matrix_example.base_matrix)
     result = matrix_example.hungarian_method()
     print("\n\nMacierz końcowa: \n", matrix_example.matrix)
